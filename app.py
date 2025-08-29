610909db73f22d204eb29cf8b8ae98cb9e3bcc1a
jpnunes012 <jpjoaopedronunes@gmail.com>
2025-08-28 23:46:19 -0300

    separando funções

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
-print('Sabor Express')
+import os
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair/n')
+def exibir_nome_do_programa():
+    print('🅢🅐🅑🅞🅡 🅔🅧🅟🅡🅔🅢🅢/n')
 
-opcao_escolhida = input ('Escolha uma opção: ')
-print ('Você escolheu a opção', opcao_escolhida)
+def exibir_opcoes():
+     print('1. Cadastrar restaurante')
+     print('2. Listar restaurantes')
+     print('3. Ativar restaurante')
+     print('4. Sair/n')
+     opcao_escolhida = input ('Escolha uma opção: ')
+     print (f'Você escolheu a opção {opcao_escolhida}')
+     opcao_escolhida = int(opcao_escolhida)
+
+def finalizar_app ():
+    os.system('cls')
+    #limpar o terminal depois de executar a função
+    print('Finalizando o App\n')
+
+def  escolher_opcao ():
+     if opcao_escolhida == 1:    
+        print('Cadastrar restaurante')
+     elif opcao_escolhida == 2:
+      print('Listar restaurantes')
+     elif opcao_escolhida == 3:
+      print('Ativar restaurante')
+     else: 
+      finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+ main()
